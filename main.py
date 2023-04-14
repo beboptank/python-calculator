e1157d88c648e4f3267fc668cdd2db6484ba383f
beboptank <beboptank12@gmail.com>
2023-04-14 14:43:52 -0400

    added for loop to display operations that can be performed by the program (+, -, *, /)

--- main.py
+++ main.py
@@ -8,7 +8,20 @@ print("*************************************")
 
 first_number = int(input("What is your first number?\n"))
 second_number = int(input("What is your second number?\n"))
+
+print("List of operations you can perform:\n")
+for i in range(1, 5):
+    if i == 1:
+        print("+")
+    elif i == 2:
+        print("-")
+    elif i == 3:
+        print("*")
+    else:
+        print("/\n")
+
 operation = input("What operation would you like to perform?\n")
+
 result = calc_functions.calculator_operations[operation](first_number, second_number)
 
 print(result)
