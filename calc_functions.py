6694b92fcd664de9aaf7a9bc304f8c86563b244e
beboptank <beboptank12@gmail.com>
2023-04-14 13:15:11 -0400

    four function definitions added to calc_functions.py (add, subtract, multiply, and divide)

---
+++ calc_functions.py
@@ -0,0 +1,18 @@
+def add(num1, num2):
+    """Adds two numbers together (num1 and num2)"""
+    return num1 + num2
+
+
+def subtract(num1, num2):
+    """Subtracts num2 from num1"""
+    return num1 - num2
+
+
+def multiply(num1, num2):
+    """Multiplies num1 by num2"""
+    return num1 * num2
+
+
+def divide(num1, num2):
+    """Divides num1 by num2"""
+    return num1 / num2
