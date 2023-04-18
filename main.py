e662bcb47c8fdddc1d639c7f050a61522dd3fc44
beboptank <beboptank12@gmail.com>
2023-04-18 12:27:58 -0400

    refactored for loop in main.py to print all operation options using calculator_operations dictionary from calc_functions.py

--- main.py
+++ main.py
@@ -10,19 +10,12 @@ first_number = int(input("What is your first number?\n"))
 second_number = int(input("What is your second number?\n"))
 
 print("List of operations you can perform:\n")
-for i in range(1, 5):
-    if i == 1:
-        print("+")
-    elif i == 2:
-        print("-")
-    elif i == 3:
-        print("*")
-    else:
-        print("/\n")
+for operation in calc_functions.calculator_operations:
+    print(operation)
 
 operation = input("What operation would you like to perform?\n")
 
 result = calc_functions.calculator_operations[operation](first_number, second_number)
 
-print(result)
+print(f"{first_number} {operation} {second_number} = {result}")
 
