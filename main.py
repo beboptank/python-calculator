d61d62d043cf3cd1227dac50d13bf8334e2c1f25
beboptank <beboptank12@gmail.com>
2023-04-18 12:49:47 -0400

    added while loop and code to continue operations with current result; added if statement to display exit message when 'n' is entered as input for continue_calculator variable (main.py)

--- main.py
+++ main.py
@@ -21,3 +21,18 @@ result = calc_functions.calculator_operations[operation](first_number, second_nu
 
 print(f"{first_number} {operation} {second_number} = {result}")
 
+continue_calculator = input("Continue using calculator with current result? Type y or n: \n")
+
+while continue_calculator == 'y':
+    new_first_number = result
+    for operation in calc_functions.calculator_operations:
+        print(operation)
+    operation = input("What operation would you like to perform?\n")
+    new_second_number = int(input("What is your second number?\n"))
+    result = calc_functions.calculator_operations[operation](new_first_number, new_second_number)
+    print(f"{first_number} {operation} {second_number} = {result}")
+    continue_calculator = input("Continue using calculator with current result? Type y or n: \n")
+
+if continue_calculator == 'n':
+    print("Calculations complete. Exiting program.")
+
