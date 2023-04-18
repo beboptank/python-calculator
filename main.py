03ae4a81acbfcef0f5a414755066aa783e648d7f
beboptank <beboptank12@gmail.com>
2023-04-18 12:34:06 -0400

    moved second_number input statement to after operation display loop (main.py)

--- main.py
+++ main.py
@@ -7,13 +7,15 @@ print("Program written by groovinflannel")
 print("*************************************")
 
 first_number = int(input("What is your first number?\n"))
-second_number = int(input("What is your second number?\n"))
+
 
 print("List of operations you can perform:\n")
 for operation in calc_functions.calculator_operations:
     print(operation)
 
 operation = input("What operation would you like to perform?\n")
+
+second_number = int(input("What is your second number?\n"))
 
 result = calc_functions.calculator_operations[operation](first_number, second_number)
 
